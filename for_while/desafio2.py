ee32698200d633ff81a8e0aeeb62dfb5d700d46c
beaadama <beatriz20familia@gmail.com>
2025-04-10 22:57:30 -0300

    Finalizar o ultimo desafio

--- for_while/desafio2.py
+++ for_while/desafio2.py
@@ -1,8 +1,29 @@
 import random
+import re
+from operator import index
+
 palavras = ['python', 'quarta', 'salgado', 'brigadeiro']
 escolha = random.choice(palavras)
+espaco = []
+b =1
+chances = 1
 
-for t in range(5):
-    len(escolha)
-    letras = len(escolha)
-    print(letras)
+len(escolha)
+print(escolha)
+for i in range(len(escolha)):
+    espaco.append('_')
+
+while chances <=5:
+    while True:
+        print(espaco)
+        char = input('Digite uma letra: ')
+        if re.findall(char, escolha):
+            print(f'A leta {char} aparece {escolha.count(char)}')
+            local = int(input('Onde deseja colocar a letra: '))
+            espaco[local] = char
+
+        else:
+            print(f'Vezes: {chances}')
+            break
+
+    chances += 1
